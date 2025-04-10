be4cf6c826d5508ca776a3242c188bbb3887d4ef
lucius <aebyl@bzz.ch>
2025-04-10 16:11:47 +0200

    About me

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-between items-center p-4 bg-purple-900 text-white">
+        <nav className="fixed top-0 left-0 w-full flex justify-between items-center p-4 bg-purple-900 text-white z-50">
             <Link href="/" className="text-lg hover:underline">Lucius Aeby</Link>
 
             <div className="ml-auto flex gap-4">
--- app/page.jsx
+++ app/page.jsx
@@ -24,12 +24,41 @@ export default function Main() {
             <div className="w-[calc(100%-10rem)] h-1 bg-purple-900 mx-20"></div>
 
 
-            <div className="text-white ">
+            <div className="text-white p-6">
                 <h1 className="flex flex-col justify-center items-center p-4 text-3xl" id="about-me">ABOUT ME</h1>
+
+                <div className="flex flex-row items-center justify-center px-10 gap-20 p-6">
+
+
+                    <Image
+                        className="rounded-md"
+                        src="/PortfolioAebyLucius.jpeg"
+                        width={300}
+                        height={400}
+                        alt="Picture of the author"
+                    />
+
+
+                    <div className="flex flex-col ">
+                        <h2 className="text-2xl mb-4">Lucius Aeby</h2>
+                        <p className="mb-2">LoremIpsum LoremIpsum LoremIpsum LoremIpsum</p>
+                        <p className="mb-2">LoremIpsum LoremIpsum LoremIpsum LoremIpsum</p>
+                        <p className="mb-2">LoremIpsum LoremIpsum LoremIpsum LoremIpsum</p>
+                        <p className="mb-2">LoremIpsum LoremIpsum LoremIpsum LoremIpsum</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center mt-6">
+            <a
+                    href="/cv.pdf"
+                    download
+                    className="bg-purple-700 hover:bg-purple-800 text-white font-semibold py-2 px-6 rounded-2xl transition duration-300"
+                >
+                    Download CV
+                </a>
             </div>
 
-
-            <div className="flex flex-col items-center justify-center text-white p-4">
+            <div className="flex flex-col items-center justify-center text-white p-6">
                 <h1 id="projects">PROJECTS</h1>
             </div>
         </div>
