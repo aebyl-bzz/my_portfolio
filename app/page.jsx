f3805ad5e908cfbdfa6e234abb6dd5f5713b16fe
lucius <aebyl@bzz.ch>
2025-08-18 11:25:58 +0200

    added more images for projects, moved project cyberattacks

--- app/page.jsx
+++ app/page.jsx
@@ -145,46 +145,6 @@ export default function Main() {
 
                 {/* Project 2 */}
                 <motion.div
-                    initial={{x: 100, opacity: 0}}
-                    whileInView={{x: 0, opacity: 1}}
-                    viewport={{once: true}}
-                    transition={{duration: 0.8}}
-                    className="flex flex-col md:flex-row-reverse items-center justify-center gap-6 md:gap-10 p-4 md:p-6 bg-purple-900/30 rounded-xl backdrop-blur-sm mb-8 w-full max-w-4xl"
-                >
-                    <div className="w-full md:w-1/2 flex flex-col text-center md:text-left justify-center">
-                        <div className="flex items-center justify-center md:justify-start gap-4 mb-2">
-                            <h2 className="text-xl md:text-2xl">project cyberattacks</h2>
-                            <a 
-                                href="https://github.com/aebyl-bzz/projekt_cyberattacks" 
-                                target="_blank" 
-                                rel="noopener noreferrer"
-                                className="text-purple-300 hover:text-purple-100 transition-colors"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-                                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
-                                </svg>
-                            </a>
-                        </div>
-                        <div className="flex flex-wrap justify-center md:justify-start gap-2 mb-4">
-                            <span className="px-2 py-1 bg-orange-500/20 text-orange-300 rounded-md text-sm">HTML</span>
-                            <span className="px-2 py-1 bg-blue-500/20 text-blue-300 rounded-md text-sm">CSS</span>
-                            <span className="px-2 py-1 bg-yellow-500/20 text-yellow-300 rounded-md text-sm">JavaScript</span>
-                        </div>
-                        <p className="mb-2 text-gray-300">A Webpage about the cyberattacks in Ukraine</p>
-                        <p className="mb-2 text-gray-300">It has many responsive features, such as an interactive map, a timeline of the attacks and more.</p>
-                    </div>
-                    <div className="w-full md:w-1/2 flex justify-center">
-                      <Image
-                        className="rounded-md hover:scale-105 transition-transform duration-300 w-full md:w-auto"
-                        src="/cyberattacks_demoImage.png"
-                        alt="Project 2"
-                        width={300}
-                        height={200}
-                      />
-                    </div>
-                </motion.div>
-                {/* Project 3 */}
-                <motion.div
                     initial={{x: -100, opacity: 0}}
                     whileInView={{x: 0, opacity: 1}}
                     viewport={{once: true}}
@@ -222,7 +182,7 @@ trains a random Forest model on games before 2020 and tests predictions on games
                       />
                     </div>
                 </motion.div>
-                {/* Project 4 */}
+                {/* Project 3 */}
                 <motion.div
                     initial={{x: 100, opacity: 0}}
                     whileInView={{x: 0, opacity: 1}}
@@ -261,7 +221,7 @@ trains a random Forest model on games before 2020 and tests predictions on games
                       />
                     </div>
                 </motion.div>
-                {/* Project 5 */}
+                {/* Project 4 */}
                 <motion.div
                     initial={{x: -100, opacity: 0}}
                     whileInView={{x: 0, opacity: 1}}
@@ -292,13 +252,56 @@ trains a random Forest model on games before 2020 and tests predictions on games
                     <div className="w-full md:w-1/2 flex justify-center">
                       <Image
                         className="rounded-md hover:scale-105 transition-transform duration-300 w-full md:w-auto"
-                        src="/elementor-placeholder-image.webp"
+                        src="/simple-banking-system.png"
                         alt="Project 1"
                         width={300}
                         height={300}
                       />
                     </div>
                 </motion.div>
+
+                {/* Project 5 */}
+                <motion.div
+                    initial={{x: 100, opacity: 0}}
+                    whileInView={{x: 0, opacity: 1}}
+                    viewport={{once: true}}
+                    transition={{duration: 0.8}}
+                    className="flex flex-col md:flex-row-reverse items-center justify-center gap-6 md:gap-10 p-4 md:p-6 bg-purple-900/30 rounded-xl backdrop-blur-sm mb-8 w-full max-w-4xl"
+                >
+                    <div className="w-full md:w-1/2 flex flex-col text-center md:text-left justify-center">
+                        <div className="flex items-center justify-center md:justify-start gap-4 mb-2">
+                            <h2 className="text-xl md:text-2xl">project cyberattacks</h2>
+                            <a 
+                                href="https://github.com/aebyl-bzz/projekt_cyberattacks" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="text-purple-300 hover:text-purple-100 transition-colors"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="flex flex-wrap justify-center md:justify-start gap-2 mb-4">
+                            <span className="px-2 py-1 bg-orange-500/20 text-orange-300 rounded-md text-sm">HTML</span>
+                            <span className="px-2 py-1 bg-blue-500/20 text-blue-300 rounded-md text-sm">CSS</span>
+                            <span className="px-2 py-1 bg-yellow-500/20 text-yellow-300 rounded-md text-sm">JavaScript</span>
+                        </div>
+                        <p className="mb-2 text-gray-300">A Webpage about the cyberattacks in Ukraine</p>
+                        <p className="mb-2 text-gray-300">It has many responsive features, such as an interactive map, a timeline of the attacks and more.</p>
+                    </div>
+                    <div className="w-full md:w-1/2 flex justify-center">
+                      <Image
+                        className="rounded-md hover:scale-105 transition-transform duration-300 w-full md:w-auto"
+                        src="/cyberattacks_demoImage.png"
+                        alt="Project 2"
+                        width={300}
+                        height={200}
+                      />
+                    </div>
+                </motion.div>
+
+
                 {/* Project 6 */}
                 <motion.div
                     initial={{x: 100, opacity: 0}}
@@ -330,7 +333,7 @@ trains a random Forest model on games before 2020 and tests predictions on games
                     <div className="w-full md:w-1/2 flex justify-center">
                       <Image
                         className="rounded-md hover:scale-105 transition-transform duration-300 w-full md:w-auto"
-                        src="/elementor-placeholder-image.webp"
+                        src="/black-jack.png"
                         alt="Project 1"
                         width={300}
                         height={300}
